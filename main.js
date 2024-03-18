6533f6899775c7b754e5139bc9d45ad8c71c83e9
eisserer <clemens.eisserer@extern.umweltbundesamt.at>
2024-03-18 17:41:35 +0100

    use map-proxy, identifier = url

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ import GeoJSON from 'ol/format/GeoJSON.js';
 import  proj4  from 'proj4/dist/proj4';
 import { register } from 'ol/proj/proj4';
 import { Circle } from 'ol/geom';
+import TileSource from 'ol/source/Tile';
+import XYZ from 'ol/source/XYZ';
 
 proj4.defs("EPSG:31287","+proj=lcc +lat_0=47.5 +lon_0=13.3333333333333 +lat_1=49 +lat_2=46 +x_0=400000 +y_0=400000 +ellps=bessel +towgs84=577.326,90.129,463.919,5.137,1.474,5.297,2.4232 +units=m +no_defs +type=crs");
 register(proj4);   
@@ -19,7 +21,10 @@ const map = new Map({
   target: 'map',
   layers: [
     new TileLayer({
-      source: new OSM(),
+      //source: new OSM(),
+      source : new XYZ({
+        url : "https://mapproxy.rest-gdi.geo-data.space/tiles/osm/webmercator/{z}/{x}/{y}.png"
+      })
     }),
     vectorLayer
   ],
@@ -33,8 +38,19 @@ const map = new Map({
 
 document.getElementById("openBtn").onclick = async function(e) {
   const identifier = document.getElementById("identifierInput").value;
-  const idParts = identifier.split(".");
-  const resultBody = await fetch("https://rewriter.rest-gdi.geo-data.space/" + idParts[1] + "/" + idParts[2] + "." + idParts[3] + "/" + idParts[4] + "/" + idParts[5] + "?outputFormat=application%2Fjson");
+  //const idParts = identifier.split(".");
+  //const resultBody = await fetch("https://rewriter.rest-gdi.geo-data.space/" + idParts[1] + "/" + idParts[2] + "." + idParts[3] + "/" + idParts[4] + "/" + idParts[5] + "?outputFormat=application%2Fjson");
+  let resultBody = null;
+  try {
+     resultBody = await fetch(identifier + "?outputFormat=application%2Fjson");
+  }catch(e) {
+  }
+
+  if(resultBody === null || !resultBody.ok) {
+    alert("Fehler beim Auflösen des Identifiers");
+    return;
+  }
+
   const json = await resultBody.json();
   
   map.removeLayer(vectorLayer);
