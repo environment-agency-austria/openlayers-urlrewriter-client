8000b7db804e3f77cfbd4c2f31d86f90bb1fbc2f
Clemens Eisserer <ceisserer@gmail.com>
2024-07-03 08:22:09 +0200

    initial PWA enablement

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
+import * as fs from 'fs';
+
 export default {
   build: {
     sourcemap: true,
 	outDir: 'docs'
   },
-  base: ''
+  base: '',
+  server: {
+    https: {
+      pfx: fs.readFileSync('./cert/localhost.p12'),
+      passphrase: ''
+    }
+  }
+
+  /*server: {
+    https: true
+  },
+  plugins: [
+    basicSsl({
+      name: 'test',
+      domains: ['*.custom.com'],
+    })]
+      */
 }
